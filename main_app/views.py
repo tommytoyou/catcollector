44b9f9ed57bf8c3b6e33dba7f02fe7c124b24f6d
Rome Bell <romebell5@gmail.com>
2021-03-19 15:21:07 -0700

    views: add contact template and function

--- main_app/views.py
+++ main_app/views.py
@@ -9,5 +9,14 @@ def about(request):
     lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a lorem non elit interdum congue ut vitae nibh. Nam maximus laoreet dui, hendrerit auctor ex convallis faucibus. Morbi posuere id nulla vitae facilisis. Morbi fermentum libero in varius malesuada. Cras condimentum lobortis neque, eu auctor dolor interdum quis. Nulla nec facilisis ante, vel efficitur nibh. Quisque venenatis augue condimentum nisi mollis rhoncus sit amet et leo.Sed eget ornare neque. Morbi pulvinar ante vitae tellus hendrerit mollis. Nullam ut gravida nulla. Donec sed dolor ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque varius risus et dui lacinia, aliquet finibus est eleifend. Ut consequat dictum lacus in elementum. Nullam pretium varius maximus. Aliquam finibus aliquet diam, quis maximus diam sollicitudin vitae. Suspendisse sed suscipit est."
     return HttpResponse(lorem_ipsum)
 
+def contact(request):
+    return render(request, 'contact.html')
+
+
 # 1. Make a view function
+# 2. Make the html page
 # 2. Add the view to the urls.py inside of main_app.urls
+
+# In browser
+# When I go to localhost:8000/contact
+# Django -> urls -> /contact -> vews.contact (runs function) -> templates -> contact.html
