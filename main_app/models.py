76453f62aca2673ad0f156a3bc0738fde2149896
Rome Bell <romebell5@gmail.com>
2021-03-19 16:54:56 -0700

    cats: add index function, urls, and views

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Cat(models.Model):
-    name = models.CharField(max_length=100)
+    age = models.IntegerField()
     breed = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    age = models.IntegerField()
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
