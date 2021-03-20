ae47e334a77a8860d1e51bf2bc559ac393a10878
Rome Bell <romebell5@gmail.com>
2021-03-19 18:26:20 -0700

    tests: add CatTestCase

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from main_app.models import Cat
+
+class CatTestCase(TestCase):
+    def setUp(self):
+        Cat.objects.create(name="Roar", breed="Tiger", description="A tiger", age=11)
+        Cat.objects.create(name="Rumble", breed="Tiger", description="A tiger", age=11)
+
+    def test_cat_names(self):
+        """Cat that are identified by name"""
+        roar = Cat.objects.get(name="Roar")
+        rumble = Cat.objects.get(name="Rumble")
+        
+        self.assertEqual(roar.name, 'Roar')
+        self.assertEqual(rumble.name, 'Rumble')
